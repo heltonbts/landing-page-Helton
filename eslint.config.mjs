179022c160a178f4d6838fa7706e9c7905fb7449
Helton Batista <heltonbts@icloud.com>
2025-08-07 11:14:19 -0300

    feat: eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,22 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+export default [
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:jsx-a11y/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended"
+  ),
+  {
+    rules: {
+      "simple-import-sort/imports": "warn",
+      "simple-import-sort/exports": "warn",
 
-export default eslintConfig;
+      "@next/next/no-html-link-for-pages": "off",
+    },
+  },
+];
